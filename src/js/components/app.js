43b87918158912e9306d4211b833112da932dc62
sue <sue@MacBook-Pro-de-sue.local>
2020-05-18 04:18:33 -0500

    debugging webpack config

---
+++ src/js/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  console.log('testing app')
+  return (
+    <div>
+      <h1>Hello oozmaKappa.js </h1>
+      <p>louis Monstruon page</p>
+    </div>
+  )
+}
+export default App;
