44ccfde30c83d1b9027678e1a602cde259461487
Tonalli López <aguilablanca_7@live.com>
2020-05-21 18:21:18 -0500

    update configuration for deployment on Zeit/now(Vercel)

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,61 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/index.js'),
   },
-  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
-    publicPath: 'http://localhost:3001/',
+    filename: 'js/[name].[hash].js',
+    publicPath: '/',
     chunkFilename: 'js/[id].[chunkhash].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: {
+          loader: 'eslint-loader',
+        },
+      },
+      {
+        test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
-        test: /\.scss$/,
+        test: /\.(sc|c)ss$/,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           'css-loader',
           'sass-loader',
         ],
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        test: /\.(png|gif|jpg|svg|webm)$/,
         use: {
           loader: 'file-loader',
           options: {
-            outputPath: 'assets/',
+            name: 'assets/images/[hash].[ext]',
+          },
+        },
+      },
+      {
+        test: /\.(woff|eot|ttf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'assets/fonts/[hash].[ext]',
           },
         },
       },
@@ -55,19 +63,47 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
+      filename: 'css/[name].[hash].css',
       chunkFilename: 'css/[id].css',
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
     }),
+    new WebpackPwaManifestPlugin({
+      name: 'Louis Monstruon',
+      short_name: 'LM',
+      description: 'set a description',
+      background_color: '#F2F6F5',
+      theme_color: '#5BDBC3',
+      inject: true,
+      ios: true,
+      icons: [
+        {
+          src: path.resolve('src/assets/images/logo_1.png'),
+          sizes: [120, 152, 167, 180, 1024],
+          destination: path.join('icons', 'ios'),
+          ios: true,
+        },
+        {
+          src: path.resolve('src/assets/images/logo_1.png'),
+          size: 1024,
+          destination: path.join('icons', 'ios'),
+          ios: 'startup',
+        },
+        {
+          src: path.resolve('src/assets/images/logo_1.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons', 'android'),
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW(),
     new webpack.DllReferencePlugin({
-      manifest: require('./modules-manifest.json'),
+      context: path.join(__dirname),
+      manifest: require('./dist/modules-manifest.json'),
     }),
     new AddAssetHtmlPlugin({
-      filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
-      outputPath: 'js',
-      publicPath: 'http://localhost:3001/js',
+      filepath: path.resolve(__dirname, './dist/*.dll.js'),
     }),
   ],
 };
--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,24 +1,25 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     modules: [
+      'prop-types',
       'react',
       'react-dom',
-      'react-router-dom',    
-    ]
+      'react-helmet',
+      'react-router-dom',
+    ],
   },
-  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].[hash].dll.js',
+    filename: '[name].[hash].dll.js',
     library: '[name]',
   },
   plugins: [
     new webpack.DllPlugin({
       name: '[name]',
-      path: path.join(__dirname, '[name]-manifest.json')
-    })
+      path: path.join(__dirname, 'dist', '[name]-manifest.json'),
+    }),
   ],
-}
+};
