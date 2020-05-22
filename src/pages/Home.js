5854e7968855b3cadad6f4823e4072babfe14f0f
sue <sue@MacBook-Pro-de-sue.local>
2020-05-22 01:57:13 -0500

    layout style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../assets/styles/main.scss';
-import logo from '../assets/images/logo_1.png';
 
 const Home = () => {
   return (
     <>
-      <div className="image-container">
-        <img src={logo} alt="logo" />
-      </div>
+      children
     </>
   );
 };
