ae34bdcc04f7b2e996572fe4ffbd1fb623c806fc
sue <sue@MacBook-Pro-de-sue.local>
2020-05-22 02:46:09 -0500

    conditionaling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, LocalGroceryStoreOutlined, NotificationsOutlined, PersonOutlined, SearchOutlined } from '@material-ui/icons';
+import { HomeOutlined,
+  LocalGroceryStoreOutlined,
+  NotificationsOutlined,
+  PersonOutlined,
+  SearchOutlined } from '@material-ui/icons';
 import '../assets/styles/components/Footer.scss';
 
-const Footer = () => {
-  return (
-    <footer className="Footer">
-      <Link className="Footer__icon" to="/">
-        <HomeOutlined />
-      </Link>
-      <Link className="Footer__icon" to="/">
-        <NotificationsOutlined />
-      </Link>
-      <Link className="Footer__icon" to="/">
-        <LocalGroceryStoreOutlined />
-      </Link>
-      <Link className="Footer__icon" to="/">
-        <SearchOutlined />
-      </Link>
-      <Link className="Footer__icon" to="/">
-        <PersonOutlined />
-      </Link>
-    </footer>
-  );
+const Footer = (props) => (
+  <>
+    {
+      props.icons ? (
+        <footer className="Footer">
+          <Link className="Footer__icon" to="/">
+            <HomeOutlined />
+          </Link>
+          <Link className="Footer__icon" to="/">
+            <NotificationsOutlined />
+          </Link>
+          <Link className="Footer__icon" to="/">
+            <LocalGroceryStoreOutlined />
+          </Link>
+          <Link className="Footer__icon" to="/">
+            <SearchOutlined />
+          </Link>
+          <Link className="Footer__icon" to="/">
+            <PersonOutlined />
+          </Link>
+        </footer>
+      ) : (
+        <footer className="Footer">
+          <Link className="Footer__terms" to="/">
+            terms and Conditions
+          </Link>
+        </footer>
+      )
+    }
+  </>
+);
+
+Footer.propTypes = {
+  icons: PropTypes.bool,
 };
 
 export default Footer;
