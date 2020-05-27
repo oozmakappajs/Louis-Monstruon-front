f29c672e9ff73a88ca41c25a8ed60eca7dcaa090
sue <sue@MacBook-Pro-de-sue.local>
2020-05-27 18:39:43 -0500

    login page

--- src/components/elements/InputComponent.js
+++ src/components/elements/InputComponent.js
@@ -4,6 +4,7 @@ import '../../assets/styles/components/elements/Input.scss';
 const InputComponent = (props) => (
   <div className="InputComponent">
     <input type="text" placeholder={props.title} className="InputComponent__input" />
+    <div className="InputComponent__line" />
   </div>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,33 @@
 import React from 'react';
-
-import logo from '../assets/images/logo_2.png';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import InputComponent from '../components/elements/InputComponent';
+import Logo from '../components/elements/Logo';
+import Button from '../components/elements/Button';
 import '../assets/styles/pages/login.scss';
 
-const Login = (props) => {
+const Login = () => {
   return (
     <div className="modalView">
-      <header className="modalView_header">
-        <h2>Only button back</h2>
-      </header>
+      <Header type="back" />
       <main className="modalView_content">
         <section className="login">
-          <img src={logo} alt="Logo" className="auth_logo" />
-          <form>
-            <input type="text" placeholder="Correo" />
-            <input type="text" placeholder="Contraseña" />
-            <button type="submit">Iniciar sesion</button>
+          <Logo />
+          <form className="login__form">
+            <div className="login__form-inputs">
+              <InputComponent title="Usuario" />
+              <InputComponent title="Nombre" />
+              <InputComponent title="Fecha de nacimiento" />
+            </div>
+            <Button name="blueBase" buttonType="submit">Siguiente</Button>
           </form>
         </section>
+        <div className="login__separator" />
         <section className="goToRegister">
-          <button type="button">Registrate</button>
-          <button type="button">Iniciar sesion con Google</button>
+          <Button name="google" buttonType="button">Registrate con </Button>
         </section>
       </main>
-      <footer className="modalView_termsAndConditions">
-        <h2>Terminos y condiciones</h2>
-      </footer>
+      <Footer />
     </div>
   );
 };
