41ece71859c291947060b0b7f1930987caa05ac8
Tonalli López <aguilablanca_7@live.com>
2020-05-28 10:09:03 -0500

    Setting containers to Settings.js

--- src/components/elements/SwitchButton.js
+++ src/components/elements/SwitchButton.js
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { BrightnessLowOutlined } from '@material-ui/icons';
+
 import '../../assets/styles/components/elements/SwitchButton.scss';
 
 const SwitchButton = (props) => {
@@ -10,17 +12,18 @@ const SwitchButton = (props) => {
           <div className="SwitchButton">
             <label className="switch">
               <input type="checkbox" />
-              <span className="slider" />
+              <span className="slider slider__theme"><BrightnessLowOutlined /></span>
             </label>
           </div>
-        ) : (
-          <div className="SwitchButton">
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider slider__off">on</span>
-            </label>
-          </div>
-        )
+        ) :
+          (
+            <div className="SwitchButton">
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider slider__off">On</span>
+              </label>
+            </div>
+          )
       }
     </>
   );
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,45 +1,12 @@
 import React from 'react';
 
+import SettingsActions from '../containers/Settings';
+
 import '../assets/styles/pages/settings.scss';
 
 const Settings = (props) => {
   return (
-    <>
-      <header className="header">
-        <h2>Header</h2>
-      </header>
-      <main className="content">
-        <section className="settings">
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-          <div>
-            <p>setting option</p>
-          </div>
-        </section>
-      </main>
-      <nav className="menu_navigation">
-        <h2>Menu de Navegación</h2>
-      </nav>
-    </>
+    <SettingsActions />
   );
 };
 
