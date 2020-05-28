6918d93ed2558255864f426a1c9a3fcbac518dc6
sue <sue@MacBook-Pro-de-sue.local>
2020-05-28 16:26:14 -0500

    solving conflicts in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
-
-=======
->>>>>>> 9df0e17554d5353a783b26849be9efa53e8acadf
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Product from './pages/Product';
@@ -21,7 +17,6 @@ import Layout from './components/Layout';
 const App = () => {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <Layout>
         <Switch>
           <Route exact path="/register" component={Register} />
@@ -38,22 +33,6 @@ const App = () => {
           <Route component={NotFound} />
         </Switch>
       </Layout>
-=======
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/notifications" component={Notifications} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/profile/:username" component={Profile} />
-        <Route exact path="/settings" component={Settings} />
-        <Route exact path="/payment" component={() => <Payment finish />} />
-        <Route exact path="/:category/:product" component={Products} />
-        <Route exact path="/:category/:product/:id" component={Product} />
-        <Route component={NotFound} />
-      </Switch>
->>>>>>> 9df0e17554d5353a783b26849be9efa53e8acadf
     </BrowserRouter>
   );
 };
