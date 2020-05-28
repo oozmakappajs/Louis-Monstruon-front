92f61b7669097be9e6b73c5d495cf2432e9cd935
Tonalli López <aguilablanca_7@live.com>
2020-05-28 10:56:23 -0500

    restructuring of NotFound.js and Search.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,9 +5,6 @@ import '../assets/styles/pages/notFound.scss';
 const NotFound = (props) => {
   return (
     <>
-      <header className="header">
-        <h2>Header</h2>
-      </header>
       <main className="content">
         <section className="notFound">
           <h1>Page Not Found</h1>
@@ -19,9 +16,6 @@ const NotFound = (props) => {
           </p>
         </section>
       </main>
-      <nav className="menu_navigation">
-        <h2>Menu de Navegación</h2>
-      </nav>
     </>
   );
 };
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,40 +5,32 @@ import '../assets/styles/pages/search.scss';
 const Search = (props) => {
   return (
     <>
-      <header className="header">
-        <h2>Header</h2>
-      </header>
-      <main className="content">
-        <section className="results">
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-          <div>
-            <p>search result</p>
-          </div>
-        </section>
-      </main>
-      <nav className="menu_navigation">
-        <h2>Menu de Navegación</h2>
-      </nav>
+      <section className="results">
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+        <div>
+          <p>search result</p>
+        </div>
+      </section>
     </>
   );
 };
