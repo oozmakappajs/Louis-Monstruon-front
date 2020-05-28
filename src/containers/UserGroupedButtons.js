c814dff9b925d8b9ed636acac94b8165a5a3d84d
sue <sue@MacBook-Pro-de-sue.local>
2020-05-28 16:28:53 -0500

    conflict in grouped buttons

--- src/containers/UserGroupedButtons.js
+++ src/containers/UserGroupedButtons.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import '../assets/styles/containers/UserGroupedButtons.scss';
@@ -12,44 +11,6 @@ const UserGroupedButtons = (props) => {
         Favoritos
       </button>
       <button type="button" className={`UserGroupedButtons__item ${active && 'UserGroupedButtons__item-active'}`} onClick={handleTabActive}>
-=======
-import React, { useState } from 'react';
-import '../assets/styles/containers/UserGroupedButtons.scss';
-
-const UserGroupedButtons = (props) => {
-  const [ActiveLeft, useIsActiveLeft] = useState(props.left);
-  const [ActiverRight, useIsActiveRight] = useState(props.right);
-
-  const handleColorChangeRight = (state) => {
-    if (state) {
-      useIsActiveLeft(false);
-      useIsActiveRight(state);
-    }
-  };
-  const handleColorChangeLeft = (state) => {
-    if (state) {
-      useIsActiveLeft(state);
-      useIsActiveRight(false);
-    }
-  };
-
-  return (
-    <div className="UserGroupedButtons">
-      <button
-        type="button"
-        onClick={() => handleColorChangeLeft(true)}
-        className={
-          `UserGroupedButtons__item ${ActiveLeft && 'UserGroupedButtons__item--active'}`
-        }
-      >
-        Favoritos
-      </button>
-      <button
-        type="button"
-        onClick={() => handleColorChangeRight(true)}
-        className={`UserGroupedButtons__item ${ActiverRight && 'UserGroupedButtons__item--active'}`}
-      >
->>>>>>> 9df0e17554d5353a783b26849be9efa53e8acadf
         historial de compra
       </button>
     </div>
