08020231b94e244166060116c8e0d3546df4906a
Tonalli López <aguilablanca_7@live.com>
2020-05-29 22:07:47 -0500

    Adding the theme change to NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { getGlobalState } from '../Context';
 
 import Button from '../components/elements/Button';
 
 import '../assets/styles/pages/notFound.scss';
 
 const NotFound = (props) => {
+  const [{ settings: { theme } }] = getGlobalState();
 
   const goToProducts = () => {
     props.history.push('/');
   };
 
   return (
-    <section className="notFound">
+    <section className={`notFound ${theme}`}>
       <h1>Upps,Page Not Found</h1>
-      <p>
+      <h3>
         404
+        {' '}
         <span role="img" aria-label="icon">
           💔😥
         </span>
-      </p>
+      </h3>
       <Button name="base-blue" action={goToProducts}>Ver más productos</Button>
     </section>
   );
