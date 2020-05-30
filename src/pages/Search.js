4238575cbbedd8c73741d728640cd1710a2fde90
Tonalli López <aguilablanca_7@live.com>
2020-05-29 21:25:22 -0500

    Adding the theme change to Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { getGlobalState } from '../Context';
+
 import SearchCard from '../components/SearchCard';
+
 import '../assets/styles/pages/search.scss';
 
 const Search = (props) => {
+  const [{ settings: { theme } }] = getGlobalState();
+
   return (
-    <>
-      <section className="results">
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-      </section>
-    </>
+    <section className={`results ${theme}`}>
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+    </section>
   );
 };
 
